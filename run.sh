ecee41a3dca9bc0bb4f2634a6027a24b56833a07
Ilya Fourmanov <ilya.fourmanov@accenture.com>
2015-12-28 02:13:11 +0000

    Support for chown of additional directroies

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 usermod -u $EXTERNAL_ID builder
 
+for FOLDER in $BUILDER_FOLDERS; do
+  chown -R builder. $FOLDER
+done
+
 echo "ENV_PATH     PATH=$PATH" >> /etc/login.defs
 su builder -c "cd /data; $*"
